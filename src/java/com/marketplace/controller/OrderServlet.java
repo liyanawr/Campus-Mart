a20fc08a2f5e9e67f983e3bf57acf9dd1b952e87
Nadiratul Liyana <liyananadiratul@gmail.com>
2026-01-16 15:47:35 +0800

    Update OrderServlet.java

--- src/java/com/marketplace/controller/OrderServlet.java
+++ src/java/com/marketplace/controller/OrderServlet.java
@@ -1,41 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.marketplace.controller;
 
-import com.marketplace.dao.*;
+import com.marketplace.dao.OrderDAO;
+import com.marketplace.model.User;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-/**
- *
- * @author Afifah Isnarudin
- */
-@WebServlet(name = "OrderServlet", urlPatterns = {"/OrderServlet"})
+@WebServlet("/OrderServlet")
 public class OrderServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("loggedUser");
+
+        if (user == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+
         String action = request.getParameter("action");
         OrderDAO dao = new OrderDAO();
-        
-        if ("updateStatus".equals(action)) {
-            int id = Integer.parseInt(request.getParameter("orderId"));
+
+        if ("placeOrder".equals(action)) {
+            try {
+                int itemId = Integer.parseInt(request.getParameter("itemId"));
+                String paymentMethod = request.getParameter("paymentMethod");
+
+                if (dao.createOrder(user.getUserId(), itemId, paymentMethod)) {
+                    // Store success message in session so it survives the redirect
+                    session.setAttribute("successMsg", "Your interest has been sent to the seller!");
+                    response.sendRedirect("dashboard.jsp");
+                } else {
+                    response.sendRedirect("checkout.jsp?id=" + itemId + "&error=Fail");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                response.sendRedirect("home.jsp");
+            }
+        } else if ("updateStatus".equals(action)) {
+            int orderId = Integer.parseInt(request.getParameter("orderId"));
             String status = request.getParameter("status");
-            dao.updateOrderStatus(id, status);
+            dao.updateOrderStatus(orderId, status);
             response.sendRedirect("seller_dashboard.jsp?status=OrderUpdated");
         } else if ("rate".equals(action)) {
             int id = Integer.parseInt(request.getParameter("orderId"));
